7f10eeaaacb0f3d89aaa79e336850fa99a92f6b1
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-06 19:40:21 -0500

    edit css

--- wk9/9.2/Llama92.js
+++ wk9/9.2/Llama92.js
@@ -6,57 +6,9 @@ Vue.component('animal-container', {
         }
     }
 });
-/* vue.component('camel-name', {
-    template: <div> 
-                <h3>{{camelname}}</h3>
-            </div>,
-    data: function() {
-        return {
-            commonName: 'Lama',
-            binomialName: 'Lama glama'
-        } */
 
 const vm = new Vue({
 el: "#app",
-    data: {
-    camelids: [
-        {
-            commonName: "Lama",
-            binomialName: "Lama glama",
-            order: "Artiodactyla",
-            family: "Camelidae",
-            genus: "Lama",
-            species: "L. glama",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
-        },
-        {
-            commonName: "Alpaca",
-            binomialName: "Vicugna pacos",
-            order: "Artiodactyla",
-            family: "Camelidae",
-            genus: "Vicugna",
-            species: "V. pacos",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
-        },
-    ]
-
-    },
-    computed: {
-        commonName: function() {
-            return this.commonName + "," + this.binomialName;
-        }
-    }
-});
-
-// instructions:
-// covert existing Vue instance into a reusable component
-// use props to pass the animal data into the component
-// then use v-for to display a component for both of the objects in the camelids array
-
-
-// Your component should go above this line.
-/* var vm = new Vue({
-    el: "#app",
     data: {
         camelids: [
             {
@@ -78,5 +30,19 @@ el: "#app",
                 image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
             },
         ]
+
+        },
+            computed: {
+            commonName: function() {
+            return this.commonName + "," + this.binomialName;
+        }
     }
-}) */
+});
+
+// instructions:
+// covert existing Vue instance into a reusable component
+// use props to pass the animal data into the component
+// then use v-for to display a component for both of the objects in the camelids array
+
+
+// Your component should go above this line.
