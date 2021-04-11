7e91e17ade23f4d839f747995c95077162d6c712
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-11 09:59:06 -0500

    sums now only in github desktop- added js file

---
+++ wk10/10.2/exercise2.js
@@ -0,0 +1,14 @@
+const vm = new Vue({
+    el: "#musicApp",
+    data: {
+        tracks: []
+    },
+    mounted () {
+        axios
+            .get('./data/music-list.json')
+            .then(response => {
+                console.log('response', response);
+            });
+    }
+
+})
