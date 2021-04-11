90e8279deeea6726478072f927574cdd2e910811
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-11 13:47:41 -0500

    added css, trouble-shooting vbind to show image

--- wk10/10.2/exercise2.js
+++ wk10/10.2/exercise2.js
@@ -1,3 +1,11 @@
+Vue.component('streaming-track', {
+    template: `<div class="track">
+                <h3>{{track.title}}</h3>
+                <div><img v-bind:src="track.cover" alt""></div>
+                <small>{{track.artist}}</small>
+    </div>`,
+    props: ['track']
+});
 const vm = new Vue({
     el: "#musicApp",
     data: {
@@ -8,6 +16,8 @@ const vm = new Vue({
             .get('./data/music-list.json')
             .then(response => {
                 console.log('response', response);
+                vm.tracks = response.data;
+                console.log(vm.tracks);
             });
     }
 
