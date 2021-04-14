eb4ae9fbc9004bfcefc5be249ca292fbfaf33288
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-14 14:04:59 -0500

    final for 10.3
    10.3

--- wk10/10.3/script.js
+++ wk10/10.3/script.js
@@ -1,8 +1,20 @@
+Vue.component('brewery-name', {
+    template: `<div class="name">
+                <h3>{{brewery.title}}</h3><br>
+                <h3>{{brewery.state}}</h3><br>
+                <small>{{brewery.city}}</small>
+    </div>`,
+    props: ['brewery', 'city', 'state']
+})
 var baseURL = "https://api.openbrewerydb.org/";
 var vm = new Vue({
     el: "#app",
     data: {
-        breweries: []
+        breweries: [],
+        name: [],
+        city: [],
+        state: [],
+        
     },
     methods: {
         getBreweries: function() {
@@ -15,4 +27,5 @@ var vm = new Vue({
     created: function() {
         this.getBreweries();
     }
+    
 });
