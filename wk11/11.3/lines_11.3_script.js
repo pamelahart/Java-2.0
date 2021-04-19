8244609f33f138b4868e8f0e149aab9f7b93c5cd
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-19 18:19:19 -0500

    11.3 setup
    setup for 11.3

---
+++ wk11/11.3/lines_11.3_script.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  let cnv = createCanvas(600, 400);
+  cnv.position(50, 50,'fixed');
+}
+
+function draw() {
+  background (0,102,153);
+  fill('white');
+  strokeWeight(2);
+  stroke('white');
+}
+
+
+
+ 
+  //for (let circleD = 16; circleD <= 36; circleD += 40) {
+    circle(circleD, 20, circleD, 20);
+  //
+  
