45d7c9fa62b920187a25895afaed42c5500334e5
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-19 18:54:47 -0500

    updated w/ info & js files

--- wk11/11.3/lines_11.3_script.js
+++ wk11/11.3/lines_11.3_script.js
@@ -11,10 +11,26 @@ function draw() {
   stroke('white');
 }
 
-
-
  
-  //for (let circleD = 16; circleD <= 36; circleD += 40) {
+/*   //for (let circleD = 16; circleD <= 36; circleD += 40) {
     circle(circleD, 20, circleD, 20);
-  //
+  // */
+
   
+/* This assignment will allow to apply some of the themes from Form and Code to real programs.
+
+Choose a theme proposed in Form and Code(Repeat Transform Parameterize Visualize or Simulate).
+    Make 3 distinct p5.js sketches that attempt to showcase that theme.
+When you're making the sketches, add in your index.html page an <h1> tag that displays which theme you are working on.  
+In your post be sure to discuss your creation:
+
+Which theme did you pick ? Why ? What drew you to that theme in particular ?
+    What p5.js tools did you use to make that theme happen ?
+        If you had more time, what else would you make ?
+            Here is an example.  (Links to an external site.)I chose to work with the "Transform" theme because I'm interested in learning about how I can take an existing form and alter it. Note that this is a pretty basic example, your assignment should try to push things a little further.  
+
+TURN IT IN
+Build the sketch on your local computer.
+Commit your sketch to Github
+Upload it to your mcad server.
+Post links to your MCAD url and Github repo here. */
