1656eb2c385bab7e34d3ff6a1a3f70436972b427
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-25 15:32:52 -0500

    11.3 creating loop for waves

--- wk11/11.3/lines_11.3_script.js
+++ wk11/11.3/lines_11.3_script.js
@@ -1,36 +1,78 @@
+// waves//
+let dot = { x: 300, y: 0 }
+let speed = 2
+let leftAnchor = []
+let rightAnchor = []
+
+// boat//
+let b1 = { x: 250, y: -10, w: 75, h: 100 }
+let b1tip = { x1: 290, y1: 100, x2: 250, y2: 30, x3: 325, y3: 30 }
+let force = 3
 
 function setup() {
-  let cnv = createCanvas(600, 400);
-  cnv.position(50, 50,'fixed');
+    let cnv = createCanvas(600, 400);
+    cnv.position(50, 50, 'fixed');
+    for (let i = 0; i < 300; i += 30) {
+        leftAnchor.push({ x: i, y: 0 })
+    }
+    for (let j = 300; j < 600; j += 30) {
+        rightAnchor.push({ x: j, y: 0 })
+    }
+
 }
 
 function draw() {
-  background (0,102,153);
-  fill('white');
-  strokeWeight(2);
-  stroke('white');
+    background(0, 102, 153);
+    strokeWeight(2);
+    stroke('white');
+    point(dot.x, dot.y)
+
+
+    // boat 1//
+    rect(b1.x, b1.y, b1.w, b1.h)
+    triangle(b1tip.x1, b1tip.y1, b1tip.x2, b1tip.y2, b1tip.x3, b1tip.y3);
+    fill(230, 12, 117)
+    b1.y += speed
+    //b1tip.x1 += speed//
+
+    for (let prop in b1tip) {
+        if (prop.includes('y')) {
+            b1tip[prop] += speed
+        }
+        // Wave LEft Loop 1//
+    }
+    for (let k = 0; k < leftAnchor.length; k++) {
+        console.log(leftAnchor[k])
+        line(leftAnchor[k].x, leftAnchor[k].y, dot.x, dot.y)
+        leftAnchor[k].x -= force
+    }
+    dot.y += speed
+    /*
+ // LEFT//  
+ // wave 1//
+   line(anchor1.x, anchor1.y, dot.x, dot.y)
+   dot.y += speed
+   anchor1.x -= force
+   
+ // wave 2//
+   line(anchor2.x, anchor2.y, dot.x, dot.y)
+   anchor2.x -= force
+ 
+   
+   
+   // RIGHT//  
+ // wave 3//  
+   line(anchor3.x, anchor3.y, dot.x, dot.y)
+   anchor3.x += force;
+   
+ // wave 4//  
+   line(anchor4.x, anchor4.y, dot.x, dot.y)
+   anchor4.x += force;
+   force = force /1
+ */
+
+    // //for (let circleD = 16; circleD <= 36; circleD += 40) {
+    //   circle(circleD, 20, circleD, 20);
+    // }//
 }
 
- 
-/*   //for (let circleD = 16; circleD <= 36; circleD += 40) {
-    circle(circleD, 20, circleD, 20);
-  // */
-
-  
-/* This assignment will allow to apply some of the themes from Form and Code to real programs.
-
-Choose a theme proposed in Form and Code(Repeat Transform Parameterize Visualize or Simulate).
-    Make 3 distinct p5.js sketches that attempt to showcase that theme.
-When you're making the sketches, add in your index.html page an <h1> tag that displays which theme you are working on.  
-In your post be sure to discuss your creation:
-
-Which theme did you pick ? Why ? What drew you to that theme in particular ?
-    What p5.js tools did you use to make that theme happen ?
-        If you had more time, what else would you make ?
-            Here is an example.  (Links to an external site.)I chose to work with the "Transform" theme because I'm interested in learning about how I can take an existing form and alter it. Note that this is a pretty basic example, your assignment should try to push things a little further.  
-
-TURN IT IN
-Build the sketch on your local computer.
-Commit your sketch to Github
-Upload it to your mcad server.
-Post links to your MCAD url and Github repo here. */
