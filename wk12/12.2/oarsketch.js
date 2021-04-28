1d00525b55e2c60c816d8370cdfbeb09ed836e94
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-04-27 22:58:15 -0500

    Nothing working. No sound. No images. No amp

--- wk12/12.2/oarsketch.js
+++ wk12/12.2/oarsketch.js
@@ -4,17 +4,36 @@ var circleX = 250
 var circleY = 250
 
 function preload() {
- mySound = loadSound('BoatPaddling.mp3');
+  soundFormats('wav');
+  mySound = loadSound('BoatPaddling.mp3');
 }
 function setup() {
   createCanvas(400, 400);
+  loadImage('OAR1.png', img => {
+    image(img, 0, 0);
+    loadImage('OAR2.png', img => {
+      image(img, 0, 30);
+      loadImage('OAR1.png', img => {
+        image(img, 0, 0);
+      });
   mySound.setVolume(0.1);
   mySound.play();
-}
+  amplitude = new p5.Amplitude();
+
+  }
+
 function draw() {
-  level = mySound.getLevel()
   var diameterX = level * 2500
   var diameterY = level * 2500 * random(1.5)
-
+  var level = amplitude.getLevel();
+  var size = map(level, 0, 1, 0, 200);
+  fill(200, 21, 20)
   ellipse(circleX, circleY, diameterX, diameterY)
 }
+/* function playWaves() {
+  if (waves.isPlaying()) {
+    waves.stop();
+  } else {
+    waves.play();
+  } */
+/* } */
