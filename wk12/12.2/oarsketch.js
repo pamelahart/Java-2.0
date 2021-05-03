3a2fa0ef212f976abebd91f8f258012bb49b3e46
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-05-02 22:48:58 -0500

    12.2 from Sketch

--- wk12/12.2/oarsketch.js
+++ wk12/12.2/oarsketch.js
@@ -1,39 +1,53 @@
-var level;
-var mySound;
-var circleX = 250
-var circleY = 250
+let img1; // Declare variable 'img'.
+let img2;
+let img3;
+let img4;
+
+//button
+var button;
+
+// class Oar {
+//  constructor(x_, y_) {
+//    // Location and size
+//    this.x = x_;
+//    this.y = y_;
+
 
 function preload() {
-  soundFormats('wav');
-  mySound = loadSound('BoatPaddling.mp3');
+ soundFormats ('wav'); 
+ mySound = loadSound('BoatPaddling.mp3');
+  
 }
 function setup() {
-  createCanvas(400, 400);
-  loadImage('OAR1.png', img => {
-    image(img, 0, 0);
-    loadImage('OAR2.png', img => {
-      image(img, 0, 30);
-      loadImage('OAR1.png', img => {
-        image(img, 0, 0);
-      });
+  createCanvas(600, 600);
+  // background(255)
   mySound.setVolume(0.1);
   mySound.play();
-  amplitude = new p5.Amplitude();
+    button = createButton('PLAY')
+  button.mousePressed(togglePlaying);
+  img1 = loadImage('OAR2.png');
+  img2 = loadImage('Waves_black.png');
+}
 
-  }
+ function loaded() {
+  
+}
+function togglePlaying() {
+  if (!mySound.isPlaying()) {
+  mySound.play();
+  mySound.setVolume(0.3);
+  button.html('PAUSE');
+}
+else{
+  mySound.pause();
+  button.html('PLAY');
+}
+  img1 = loadImage('OAR2.png');
+  img2 = loadImage('Waves_black.png')
+}
 
 function draw() {
-  var diameterX = level * 2500
-  var diameterY = level * 2500 * random(1.5)
-  var level = amplitude.getLevel();
-  var size = map(level, 0, 1, 0, 200);
-  fill(200, 21, 20)
-  ellipse(circleX, circleY, diameterX, diameterY)
+  image(img1, 200, 0, 300);
+ 
 }
-/* function playWaves() {
-  if (waves.isPlaying()) {
-    waves.stop();
-  } else {
-    waves.play();
-  } */
-/* } */
+
---
+++ wk12/more save/music1_script.js
@@ -0,0 +1,13 @@
+var mySound;
+
+function preload() 
+
+function setup() {
+    createCanvas(400, 400);
+    mySound = loadSound('music/boatPaddling.mp3);
+}
+
+function draw() {
+    background(220);
+    
+}
