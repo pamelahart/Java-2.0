cc1bc50a13cb11dfb9367e81396994e40f9c8970
pamelahart <57786156+pamelahart@users.noreply.github.com>
2021-05-09 09:43:43 -0500

    added in phases - not correct - need edits

--- wk14/MAIN_wave1_sketch.js
+++ wk14/MAIN_wave1_sketch.js
@@ -39,6 +39,22 @@ var boat2 = new Boat(820, 280, '#eb8034', 'up', speed2)
 var boat3 = new Boat(1020, 450, '#7333b8', 'up', speed3)
 // UP is our boat direction & can be changed to down & is left & right, speed will be forward only */
 
+//Create Game State//
+let raceState = {
+  //0 for initializing
+  //1 for push the mouseButton
+  //2 end the race
+  phase: 0,
+  currentRaceIndex: 0 //start the race
+};
+let race = [
+  {phase1: 'phase 1', phase2: 'phase2', phase3: 'phase 3'},
+  {phase2: 'phase 2', phase2: 'phase2', phase3: 'phase 3'},
+  {phase3: 'phase 1', phase2: 'phase2', phase3: 'phase 3'}
+];
+let raceBegin = []; // wave starts moving once button is pressed
+let startGun = new StartSound // starting gun sound
+let button;
 
 //PREload music - SOUND CANCELLED OUT BOAT RACE - BUTTON NOT IN RIGHT PLACE NOT WORKING!!!//
 function preload() {
@@ -48,6 +64,10 @@ function preload() {
 function setup() {
   createCanvas(1800, 600);
   background('#b0f0f7');
+  button = createButton('begin');
+  //console.log(button)
+  button.position((width / 2) - (button.width/2), height /2 - (button.height /2));
+  button.mousePressed(startGun);
   mySound.setVolume(0.1);
   mySound.play();
   button = createButton('START');
@@ -74,7 +94,50 @@ function togglePlaying() {
 }  
 
 function draw() { 
+  //adding in new sequence start//
+  if(raceState.phase > 0 && gameState.phase < 3) {
+    pink.show(); //not correct name
+    purple.show(); //not correct name
+    start(race[raceState.currentRaceIndex].question, 0, 200, width,300) //not correct action
+  }
+  if(raceState.phase === 1) {
+    text(race[raceState.currentRaceIndex].first, 0, 300, width, 400) //not correct action
+  } 
   
+  if (raceState.phase === 2) {
+    text(race[raceState.currentRaceIndex].second, 0, 300, width, 400) //not correct action
+  }
+
+  if (raceState.phase === 3) {
+    text(race[raceState.currentRaceIndex].third, 0, 300, width, 400)
+  } //not correct action
+
+  }
+  function startRace() {
+    raceState.phase1 = 1
+    button.position(-100, -100) //not correct action
+  }
+  function mousePressed() {
+    if (startRace(pink) && raceState.phase > 0) {
+      startSound(pink) //not correct action
+  }
+    if (startRace(purple) && raceState.phase > 0) {
+      startRace(purple) //not correct action
+  }
+  raceEnd.push() // do I even need this push???
+
+  if (order === "first") {
+    console.log ('switch to second')
+    race.State.phase = 2;
+  } else if (order === 'second') {
+    quizState.currentRaceIndex++;
+    quizState.phase = 1;
+  }
+ else {
+  // race is done
+  raceState.phase = 3
+}
+
   stroke('#006699');
   //save these colors for drawing all the blues
   // color('#b0f0f7'); light blue
@@ -83,7 +146,7 @@ function draw() {
 
   noFill();
  
-beginShape();
+  beginShape();
   vertex(0, height);
   for(var x = 0; x < width; x++){
     //var angle = map(x, 0, width, 0, TWO_PI);
